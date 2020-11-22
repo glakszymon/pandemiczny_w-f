414f77fe72880a939dab2394bbf7b02686fc89a9
Szymon Gawrylak <szymon.g.fon@gmail.com>
2020-11-22 21:24:50 +0100

    dobra noc

---
+++ script/engine_cwiczen.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    exerciseList: [],
+    currentFile: null
+  },
+  methods: {
+    init: function() {
+        this.laduj_liste();
+    },
+
+  
+    laduj_liste: function () {
+      axios.get('data/list.json')
+      .then(function (response) {
+        lista = response.data;
+        lista.sort(function(a,b){
+            if (a.name < b.name)
+              return -1;
+            if (a.name > b.name)
+              return 1;
+            return 0;
+        });
+        app.exerciseList = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    opis_cwiczenia: function (exercise){
+      //console.log(exercise);
+      let currentExercise = exercise;
+      axios.get('exercise/'+ exercise.file)
+      .then(function (response) {
+        var converter = new showdown.Converter(),
+        html = converter.makeHtml(response.data);
+        document.getElementById('content').innerHTML = html;
+      
+        app.currentFile = currentExercise.file;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  }
+})
+
+
+
+app.init();
