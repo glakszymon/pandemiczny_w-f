b1aea517fc9b824c9936dc268283551ea295fc48
Unknown <szymek@elita.localdomain>
2020-11-28 21:20:16 +0100

    zxczxc

--- script/engine_cwiczen.js
+++ script/engine_cwiczen.js
@@ -90,6 +90,7 @@ var app = new Vue({
     
     finish: function() {
       this.nazwa_cwiczenia_teraz = "To juz jest koniec nie ma już nic";
+      mode = 'finish'
     },
     
     count_down: function() {
