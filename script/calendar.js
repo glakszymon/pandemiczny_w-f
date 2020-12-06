4d66a180845d6d640663c287cf864789db6d19f7
Unknown <szymek@elita.localdomain>
2020-12-06 19:54:04 +0100

    asa

--- script/calendar.js
+++ script/calendar.js
@@ -1,13 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    currentday: null
+    mode: "kalendarz",
+    currentday: null,
+    slide_file: null
   },
   methods: {
     init: function() {
         var today = new Date();
         this.currentday = today.getDate();
+    },
+  
+    show_slide: function(number) {
+      this.slide_file = "data/kalendarz_sportu/slide"+number+".jpg";
+      this.mode = 'slide';
     }
+
   }
 })
 
